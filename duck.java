19175e12593aa813a36eb5086a2a732f1bc3179c
Stephen Veals <sveals@umich.edu>
2018-10-11 15:20:11 -0400

    New version

---
+++ duck.java
@@ -0,0 +1,11 @@
+public class duck{
+  public fly(){
+    system.out.println("A duck can fly");
+  }
+  public swim(){
+    system.out.println("A duck can swim");
+  }
+  public eat(){
+    system.out.println("A duck can eat");
+  }
+}
